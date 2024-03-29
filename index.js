a9f6eb13eec15f8ef8f2b18a49afd924e47a5cec
Chandan Kumar <149804185+chandankumarm55@users.noreply.github.com>
2024-03-29 16:16:19 +0530

    changes

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-const express = require('express')
-const cors = require("cors")
-const mongoose = require("mongoose")
-const router = require('./routes/UserRoute')
-const dotenv = require("dotenv")
-dotenv.config()
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const router = require('./routes/UserRoute');
+const dotenv = require('dotenv');
 
+dotenv.config();
 
-app = express()
-PORT = 5000
-app.use(express.json())
+const app = express();
 
+// Middleware
 app.use(cors());
+app.use(express.json());
 
+// Routes
+app.use('/api/user', router);
 
-app.listen(PORT, () => {
-    console.log(`server is running ar PORT ${PORT}`)
-})
+// Start server and connect to MongoDB
 
-app.use('/api/user', router)
-mongoose.connect(process.env.MONGOURL).then(
-    console.log("mongo db is connected")
-)
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running at PORT ${process.env.PORT}`);
+});
+
+mongoose.connect(process.env.MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MongoDB connected successfully");
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error);
+    });
